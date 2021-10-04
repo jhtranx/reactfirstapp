1e11efdd50b12d02f622afa70079cdc34bd60ef9
Justin Tran <jhtranx@gmail.com>
2021-10-04 11:12:34 -0700

    part 4 and 5 complete

--- backend/sample_backend.py
+++ backend/sample_backend.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+from random import randint, randrange
 
 
 app = Flask(__name__)
@@ -43,13 +44,23 @@ users = {
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users/<id>/')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
     if id :
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return ({})
+        if request.method == 'GET':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        elif request.method == 'DELETE':
+            for user in users['users_list']:
+                if user['id'] == id:
+                    userToRem = user
+            users['users_list'].remove(userToRem)
+            resp = jsonify(success = True)
+            resp.status_code = 204 #optionally, you can always set a response code. 
+            # 200 is the default code for a normal response
+            return resp
     return users
 
 @app.route('/users', methods=['GET', 'POST', 'DELETE'])
@@ -74,16 +85,15 @@ def get_users():
     elif request.method == 'POST':
         userToAdd = request.get_json()
         users['users_list'].append(userToAdd)
-        resp = jsonify(success=True)
-        #resp.status_code = 200 #optionally, you can always set a response code. 
+        userToAdd['id'] = randID()
+        resp = jsonify(userToAdd) # send back users after adding id
+        resp.status_code = 201 #optionally, you can always set a response code. 
         # 200 is the default code for a normal response
         return resp
 
-    elif request.method == 'DELETE':
-        userToRem = request.get_json()
-        users['users_list'].remove(userToRem)
-        resp = jsonify(success=True)
-        #resp.status_code = 200 #optionally, you can always set a response code. 
-        # 200 is the default code for a normal response
-        return resp
+    
+
+def randID():
+    return str(randint(100, 999))
+
 
--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,29 +1,31 @@
-import React, {useState} from 'react';
 import React, {useState, useEffect} from 'react';
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList} />
-    </div>
-  )
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+          setCharacters(result);
+    });
+  }, [] );
+  
   
   function removeOneCharacter (index) {
-    const updated = characters.filter((character, i) => {
-        return i !== index
-      });
-      setCharacters(updated);
+    try {
+      makeDeleteCall(index)
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+
 
   async function fetchAll(){
     try {
@@ -35,14 +37,48 @@ function MyApp() {
        console.log(error); 
        return false;         
     }
- }
+  }
 
- useEffect(() => {
-  fetchAll().then( result => {
-     if (result)
-        setCharacters(result);
-   });
-}, [] );
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+
+  async function makeDeleteCall(index){
+    const id = characters[index].id
+    console.log(id)
+    const response = await axios.delete('http://localhost:5000/users/' + id);
+    console.log(response.status)
+
+    if (response.status === 204) {
+      const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+    return response;
+  }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result)
+      setCharacters([...characters, result.data] );
+    });
+  }
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
 
 }
 
